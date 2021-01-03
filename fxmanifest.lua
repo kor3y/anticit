df03b708b27564bf9b2866e4d65f910753f8ea0a
korey <75332399+kor3y@users.noreply.github.com>
2021-01-03 19:33:32 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,5 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'korey#5241'
-
 client_scripts{'lib/*', 'cfg/*', 'client/*'}
 server_scripts{'@vrp/lib/utils.lua', '@mysql-async/lib/MySQL.lua','server/*'}
